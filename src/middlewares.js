5f99c0129211111873702a2694d1d4ffaabc01df
Rafael Santos <rafaelcachorroquentefoda@gmail.com>
2025-11-30 15:48:40 -0300

    feat: create middleware,  config the port and bodyParser

---
+++ src/middlewares.js
@@ -0,0 +1,7 @@
+const bodyParser = require("body-parser");
+
+module.exports = (app) => {
+  app.set("port", process.env.PORT || 3000);
+  app.set("json spaces", 4);
+  app.use(bodyParser.json());
+};
