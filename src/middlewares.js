ae7d6eceac20771cd801e3421619085a027c566f
Rafael Santos <rafaelcachorroquentefoda@gmail.com>
2025-11-30 18:59:51 -0300

    feat: create app.use for authenticate

--- src/middlewares.js
+++ src/middlewares.js
@@ -4,6 +4,8 @@ module.exports = (app) => {
   app.set("port", process.env.PORT || 3000);
   app.set("json spaces", 4);
   app.use(bodyParser.json());
+  // Middleware de autenticação
+  app.use(app.auth.authenticate);
   // Middleware para remover o id do body
   app.use((req, res, next) => {
     if (req.body && req.body.id) {
