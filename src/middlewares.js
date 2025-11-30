efac9bd81aeceda67ee6e583288ce583c47cd264
Rafael Santos <rafaelcachorroquentefoda@gmail.com>
2025-11-30 17:50:17 -0300

    feat: implements app.use for remove body.id

--- src/middlewares.js
+++ src/middlewares.js
@@ -4,4 +4,11 @@ module.exports = (app) => {
   app.set("port", process.env.PORT || 3000);
   app.set("json spaces", 4);
   app.use(bodyParser.json());
+  // Middleware para remover o id do body
+  app.use((req, res, next) => {
+    if (req.body && req.body.id) {
+      delete req.body.id;
+    }
+    next();
+  });
 };
