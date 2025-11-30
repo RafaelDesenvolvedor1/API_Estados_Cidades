a41b2c30d64504455b02fcb557c07e7842abbad7
Rafael Santos <rafaelcachorroquentefoda@gmail.com>
2025-11-30 16:10:25 -0300

    chore: inject dependencies with Consign

---
+++ index.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const express = require("express");
+const consign = require("consign");
+
+const app = express();
+
+consign({ cwd: "src" })
+  .include("db.js")
+  .then("models")
+  .then("associations.js")
+  .then("middlewares.js")
+  .then("server.js")
+  .into(app);
